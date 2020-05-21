3f8cbcd0d7628f7b3500739d348f70081dc33bba
vserifsaglam <vahit.serif119@gmail.com>
2020-05-21 16:07:58 +0300

    added FormRequest.php file

---
+++ src/LumenFormRequest/FormRequest.php
@@ -0,0 +1,152 @@
+<?php
+
+
+namespace Owuan\LumenFormRequest;
+
+
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class FormRequest extends Request
+{
+
+    /**
+     * Validate the class instance.
+     *
+     * @return void
+     * @throws AuthorizationException
+     * @throws ValidationException
+     */
+    public function validateResolved()
+    {
+
+        $this->prepareForValidation();
+
+        if (!$this->passesAuthorization()) {
+            $this->failedAuthorization();
+        }
+
+        $instance = $this->getValidatorInstance();
+
+        if ($instance->fails()) {
+            $this->failedValidation($instance);
+        }
+
+        $this->passedValidation();
+    }
+
+    /**
+     * @return Validator
+     */
+    public function getValidatorInstance()
+    {
+        $app = app();
+
+        return \Illuminate\Support\Facades\Validator::make($this->validationData(),
+            $app->call([$this, 'rules']),
+            $app->call([$this, 'messages']),
+            $app->call([$this, 'attributes'])
+        );
+    }
+
+    /**
+     * Handle a passed validation attempt.
+     *
+     * @return void
+     */
+    protected function passedValidation()
+    {
+        //
+    }
+
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        //
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw (new ValidationException($validator));
+    }
+
+    /**
+     * Get data to be validated from the request.
+     *
+     * @return array
+     */
+    protected function validationData()
+    {
+        return $this->all();
+    }
+
+    /**
+     * Determine if the request passes the authorization check.
+     *
+     * @return bool
+     */
+    protected function passesAuthorization()
+    {
+        if (method_exists($this, 'authorize')) {
+            return app()->call([$this, 'authorize']);
+        }
+
+        return true;
+    }
+
+    /**
+     * Handle a failed authorization attempt.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    protected function failedAuthorization()
+    {
+        throw new AuthorizationException('This action is unauthorized.');
+    }
+
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return [];
+    }
+
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [];
+    }
+}
